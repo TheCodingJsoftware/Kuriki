eca0dd2ef2cc5da17090fd6c10b5dbfcd84452bd
Jared Gross <jaredgrozz@gmail.com>
2025-09-24 10:05:39 -0500

    Added install

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -4,8 +4,10 @@ import "@static/css/style.css"
 import "beercss"
 import "material-dynamic-colors"
 import { AppearanceDialog } from "@components/common/dialogs/appearance-dialog";
+import { initInstall } from "@utils/install";
 
 document.addEventListener("DOMContentLoaded", () => {
+    initInstall();
     const appearanceButton = document.getElementById("appearance-button") as HTMLButtonElement;
     appearanceButton.addEventListener("click", () => {
         new AppearanceDialog();
