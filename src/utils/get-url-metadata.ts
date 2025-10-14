c072ccc97062d2c792c94547ffa0f7ae5c22f9d6
Jared Gross <jaredgrozz@gmail.com>
2025-10-13 20:34:01 -0500

    fix: update fetch URL in getUrlMetadata function to use secure HTTPS endpoint

--- src/utils/get-url-metadata.ts
+++ src/utils/get-url-metadata.ts
@@ -1,6 +1,6 @@
 export async function getUrlMetadata(url: string) {
     try {
-        let response = await fetch(`http://10.10.10.10:5055/metadata?url=${encodeURIComponent(url)}`);
+        let response = await fetch(`https://cert.pinelandfarms.ca/metadata?url=${encodeURIComponent(url)}`);
         let data = await response.json();
         if (data.error) {
             return url;
